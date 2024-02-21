5eda6b489260823a7fe3c100629597a40aeb455c
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-02-21 15:58:23 +0900

    ㄷㄷ

---
+++ day01/C_Project01/C_Project01/test04.cpp
@@ -0,0 +1,18 @@
+// 반복 제어문: for문
+
+#include <iostream>
+
+using namespace std;
+// int sum;
+int main()
+
+{
+	int sum = 0; //2
+	for (int i = 1; i <= 100; i++) {
+		sum += i;
+   }
+	cout << sum << endl;
+
+	
+	return 0;
+}
---
+++ day01/C_Project01/C_Project01/test05.cpp
@@ -0,0 +1,26 @@
+// 반복 제어문 : while
+
+
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int i = 1;
+
+	while (true)
+
+	{
+		i = i + 1;
+	
+		
+
+
+	}
+
+
+
+return 0;
+}
