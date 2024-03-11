5e6cc8fa17367388e23546da2e7673e9c719a9d9
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-03-11 12:25:30 +0900

    ㅜㅜ

--- day13/Project13/Project13/test03_vector02.cpp
+++ day13/Project13/Project13/test03_vector02.cpp
@@ -7,18 +7,25 @@ int main(void)
 
 	vector<int> v1;
 	int in;
+	int sum = 0;
 
-	cout << "정수를 입력하시오 0은 종료 :";
-	cin >> in;
+	
 
 
 	while (true)
 	{
+		cout << "정수를 입력하시오 0은 종료 :";
+		cin >> in;
 		if (in == 0) {
 			break;
 		}
 		v1.push_back(in);
+		sum += in;
+		cout << sum / v1.size() << endl;
 	}
+	cout << "프로그램 종료." << endl;
+
+
 
 
 
