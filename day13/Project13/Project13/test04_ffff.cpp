92ae2958a43e36898500fba022fffb69968c5d1f
KimJaeRin <158007433+KimJaeRin@users.noreply.github.com>
2024-03-12 17:15:59 +0900

    eeeee

---
+++ day13/Project13/Project13/test04_ffff.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+
+{
+	char ary[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+
+
+	int len = sizeof(ary) / sizeof(ary[0]);
+
+	int i = 0;
+
+	while (i<len)
+	{
+	if 
+
+
+	}
+
+
+
+
+
+
+
+	return 0;
+}
